1fcbf7affb90df24e3a3a669c440fc825621afb8
fisher1031 <fisher1031@outlook.jp>
2021-03-08 21:21:36 +0900

    AjaxAppの準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+// 「..」は、「一つ上の階層」を意味する
+require("../memo")  // 追記する
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
+# AjaxAppは、投稿されたメモ一覧をトップページに表示する仕様
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'  
   post 'posts', to: 'posts#create'
 end
