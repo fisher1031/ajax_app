5f4e94dd48657bfa785af20e96a26984ae1a08a4
fisher1031 <fisher1031@outlook.jp>
2021-03-09 12:26:53 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html
+}
+
 function post (){
   // リクエストを送信する処理
   // getElementByIdメソッドで取得した投稿ボタンの要素を変数submitに格納
@@ -23,6 +37,40 @@ function post (){
     XHR.responseType = "json";
     // sendメソッドはリクエストを送信するメソッド
     XHR.send(formData);
+    // レスポンスの受信に成功したときの処理
+    XHR.onload = () => {
+      // XHR.statusには、HTTPステータスコードが格納
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        // return null;によってJavaScriptの処理から抜け出す
+        // エラーが出た場合に、30行目以降に記述されている処理を行わないようにすることが目的
+        return null;
+      }
+      // 新しいメモを挿入するための要素を取得して、変数listに格納
+      const list = document.getElementById("list");
+      // リセットの対象となるフォームの要素contentを取得して、変数formTextに格納
+      const formText = document.getElementById("content");
+      // 関数buildHTMLの返り値にhtmlを指定
+      // htmlとは、3〜11行目で定義した変数htmlのことを表している。つまり、投稿後に新たに生成されたHTML
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // XHR.response.postと記述することで、レスポンスの中から投稿されたメモの情報を抽出し、変数itemに格納
+      // XHR.response.postで値が取れるのは、postsコントローラーのcreateアクションにrender json: {post: post}と記述されていることで、postというキーと投稿されたメモの内容が紐付いているから
+      const item = XHR.response.post;
+      // item内に格納されたメモの情報を元にして、ブラウザに描画するためのHTMLを生成し、変数htmlに格納
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+        // insertAdjacentHTMLメソッドの第一引数にafterendを指定することで、変数listに格納された要素の直後に生成したHTMLを挿入
+        list.insertAdjacentHTML("afterend", html);
+        // formTextのvalue属性に空の文字列を指定することで、フォームの中身を毎度リセット
+        formText.value = "";
+    };
   });
  }
  // ページが読み込まれることをトリガーにして、処理が実行されるように関数を定義
