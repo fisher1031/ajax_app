1b75a56d932843397ab3e09ad2d1eb644e9a32e4
fisher1031 <fisher1031@outlook.jp>
2021-03-09 11:24:02 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,12 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    # 新たに投稿されたメモの内容を変数postに格納
+    post = Post.create(content: params[:content])
+    # renderメソッドとは、呼び出すテンプレート（ビュー）ファイルを指定するメソッド
+    # json:の部分をjsonオプションといい、これを指定することによって、
+    # 直後に記述した{ post: post }というデータをJSON形式で返却
+    # 12行目で定義した変数postの値を、postというキーとセットでJavaScriptに送信
+    render json:{ post: post }
   end
 end
